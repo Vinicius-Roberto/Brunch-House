541523b464ee47e33973ce3fc5435dff416e95be
Marllonv <marllonvicente20@gmail.com>
2024-12-02 21:20:37 -0300

    Update Layout (Espero que seja o ultimo)

--- app.py
+++ app.py
@@ -1,15 +1,16 @@
 import os
 from flask import Flask, render_template
 
-app = Flask(__name__i)
+app = Flask(__name__)
 
 @app.route("/")
+@app.route("/index")
 def index():
     return render_template("index.html")
 
 @app.route("/produtos")
 def produtos():
-    return render_template("produtos.html")
+    return render_template("/produtos.html")
 
 if __name__ == "__main__":
     app.run(debug=True)
